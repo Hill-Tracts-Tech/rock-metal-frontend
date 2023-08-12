d34d16c86ec0ae87c275aca30b3a8dc9b280d201
Hill-Tracts-Tech <chttravel.htt@gmail.com>
2023-08-12 14:03:42 +0600

    wip

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,6 +2,22 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { mobile } from "../responsive";
 
+const CategoryItem = ({ item }) => {
+  return (
+    <Container>
+      <Link to={`/products/${item.cat}`}>
+        <Image src={item.img} />
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
+      </Link>
+    </Container>
+  );
+};
+
+export default CategoryItem;
+
 const Container = styled.div`
   flex: 1;
   margin: 3px;
@@ -41,19 +57,3 @@ const Button = styled.button`
   cursor: pointer;
   font-weight: 600;
 `;
-
-const CategoryItem = ({ item }) => {
-  return (
-    <Container>
-      <Link to={`/products/${item.cat}`}>
-        <Image src={item.img} />
-        <Info>
-          <Title>{item.title}</Title>
-          <Button>SHOP NOW</Button>
-        </Info>
-      </Link>
-    </Container>
-  );
-};
-
-export default CategoryItem;
