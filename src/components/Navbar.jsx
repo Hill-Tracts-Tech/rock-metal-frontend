a8b444bd229ea8e48cef38f0d814e76ff73edb32
UhaiMong <uhaimarma2016@gmail.com>
2023-08-12 14:40:52 +0600

    wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,63 @@
 import { Badge } from "@material-ui/core";
-import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import {
+  Search,
+  ShoppingCartOutlined,
+  AccountCircle,
+} from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 import { mobile } from "../responsive";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Logo>ROCK METAL</Logo>
+        </Left>
+        <Center>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <SearchButton>
+              <Search
+                style={{
+                  color: "white",
+                  fontSize: 20,
+                  textAlign: "center",
+                }}
+              />
+            </SearchButton>
+          </SearchContainer>
+        </Center>
+        <Right>
+          <Link to="/account">
+            <MenuItem>
+              <AccountCircle />
+            </MenuItem>
+          </Link>
+          <Link to="/register">
+            <MenuItem>REGISTER</MenuItem>
+          </Link>
+          <Link to="/login">
+            <MenuItem>SIGN IN</MenuItem>
+          </Link>
+          <Link to="/cart">
+            <MenuItem>
+              <Badge badgeContent={quantity} color="primary">
+                <ShoppingCartOutlined />
+              </Badge>
+            </MenuItem>
+          </Link>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
 
 const Container = styled.div`
   height: 60px;
@@ -35,15 +88,29 @@ const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
   display: flex;
   align-items: center;
+  border-radius: 18px;
   margin-left: 25px;
-  padding: 5px;
+  padding: 3px;
 `;
 
 const Input = styled.input`
   border: none;
+  outline: none;
+  flex: 1;
+  background: transparent;
   ${mobile({ width: "50px" })}
 `;
 
+const SearchButton = styled.button`
+  color: #757171;
+  border: none;
+  border-radius: 100%;
+  padding: 6px 8px;
+  cursor: pointer;
+  &:hover {
+    background-color: #17af26;
+  }
+`;
 const Center = styled.div`
   flex: 1;
   text-align: center;
@@ -67,40 +134,3 @@ const MenuItem = styled.div`
   margin-left: 25px;
   ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
-
-const Navbar = () => {
-  const quantity = useSelector((state) => state.cart.quantity);
-  return (
-    <Container>
-      <Wrapper>
-        <Left>
-          <Language>EN</Language>
-          <SearchContainer>
-            <Input placeholder="Search" />
-            <Search style={{ color: "gray", fontSize: 16 }} />
-          </SearchContainer>
-        </Left>
-        <Center>
-          <Logo>ROCK METAL</Logo>
-        </Center>
-        <Right>
-          <Link to="/register">
-            <MenuItem>REGISTER</MenuItem>
-          </Link>
-          <Link to="/login">
-            <MenuItem>SIGN IN</MenuItem>
-          </Link>
-          <Link to="/cart">
-            <MenuItem>
-              <Badge badgeContent={quantity} color="primary">
-                <ShoppingCartOutlined />
-              </Badge>
-            </MenuItem>
-          </Link>
-        </Right>
-      </Wrapper>
-    </Container>
-  );
-};
-
-export default Navbar;
