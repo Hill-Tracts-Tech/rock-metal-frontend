8f64b0415cbd7a3269351af2227988ae3ac8a20a
Rasalahmad <rasalahmadsit@gmail.com>
2023-10-12 21:18:22 +0600

    wip

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from "redux-persist/integration/react";
+
+if (process.env.REACT_APP_PRODUCTION === "YES") console.log = () => {};
 
 ReactDOM.render(
   <Provider store={store}>
